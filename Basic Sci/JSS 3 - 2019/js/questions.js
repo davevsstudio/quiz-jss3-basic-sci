dce2ee8d86d1aa30ebfea31518dd9a26275b3086
davestar <142616037+davevsstudio@users.noreply.github.com>
2024-09-12 21:46:45 -0700

    Add files via upload

---
+++ Basic Sci/JSS 3 - 2019/js/questions.js
@@ -0,0 +1,93 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Which of the following financial institutions accepts and keeps deposits for customers?",
+    answer: "Commercial banks",
+    options: [
+      "Central bank",
+      "Commercial banks",
+      "Finance houses",
+      "Insurance companies"
+    ]
+  },
+    {
+    numb: 2,
+    question: "The market for lending and borrowing of long-term loans is the market.",
+    answer: "commodity",
+    options: [
+      "capital",
+      "commodity",
+      "consumer",
+      "money"
+    ]
+  },
+    {
+    numb: 3,
+    question: "The paths through which goods pass from producers to final consumers is?",
+    answer: "channel of distribution",
+    options: [
+      "channel of distribution",
+      "dissemination route",
+      "post-sale service",
+      "pre-sale activity"
+    ]
+  },
+    {
+    numb: 4,
+    question: "An expression of feeling of dissatisfaction about a business activity is?",
+    answer: "complaint",
+    options: [
+      "complaint",
+      "exoneration",
+      "opportunity",
+      "threat"
+    ]
+  },
+    {
+    numb: 5,
+    question: "The following are reasons for consumer protection except",
+    answer: "encouraging the exploitation of consumers",
+    options: [
+      "encouraging the exploitation of consumers",
+      "ensuring regular supply of essential commodities",
+      "guaranteeing goods and services rendered are of high quality",
+      "making sure that consumers are satisfied"
+    ]
+  },
+  {
+    numb: 6,
+    question: "The maximum number of individuals that can form a private company is",
+    answer: "20",
+    options: [
+      "10",
+      "28",
+      "20",
+      "70"
+    ]
+  },
+
+  {
+    numb: 7,
+    question: "Granting discount to a buyer of a product is an example of",
+    answer: "pre-sale service",
+    options: [
+      "after-sales service",
+      "customer care",
+      "pre-sale service",
+      "public relations"
+    ]
+  },
+
+  {
+    numb: 8,
+    question: "The reward for capital as a factor of production is",
+    answer: "rent",
+    options: [
+      "interest",
+      "profit",
+      "rent",
+      "salary"
+    ]
+  },
+];
